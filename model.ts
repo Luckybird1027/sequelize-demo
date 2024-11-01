8760f9e69823751bf154ae655bbd869d592eb5d6
Luckybird <1342979416@qq.com>
2024-11-01 19:27:40 +0800

    编写create接口

---
+++ model.ts
@@ -0,0 +1,36 @@
+import { Sequelize, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize"
+
+export const sequelizeInstance = new Sequelize("postgres://mimir:mimir-pwd@localhost:5432/test")
+
+export interface UserPO extends Model<InferAttributes<UserPO>, InferCreationAttributes<UserPO>> {
+    id: number;
+    username: string;
+    email: string | null;
+    telephone: string | null;
+};
+
+export const UserModel = sequelizeInstance.define<UserPO>(
+    "user",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING(64),
+        },
+        email: {
+            type: DataTypes.STRING(128),
+            allowNull: true,
+        },
+        telephone: {
+            type: DataTypes.STRING(32),
+            allowNull: true,
+        }
+    },
+    {
+        tableName: "user",
+    }
+);
+await UserModel.sync();
--- setup.ts
+++ setup.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { createLogger } from "@liplum/log";
+import { UserModel, UserPO } from "./model.js";
 
 export const log = createLogger();
 export const app: express.Express = express();
@@ -11,3 +12,15 @@ app.use(cookieParser());
 const v1 = express.Router();
 app.use("/v1", v1);
 v1.use("/user", userRouter);
+
+userRouter.post("/", async (req, res) => {
+    const userPO: UserPO = req.body;
+    await UserModel.create(userPO)
+        .then(() => {
+            res.status(201).json(userPO);
+        })
+        .catch((error) => {
+            log.error(error);
+            res.status(500).json({ error: "Internal Server Error" });
+        })
+})
