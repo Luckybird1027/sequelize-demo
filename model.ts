4d3e1c9c6749dc40a67e007c96431529cd4aeb9b
Luckybird <1342979416@qq.com>
2024-11-01 23:10:14 +0800

    实现update接口

--- model.ts
+++ model.ts
@@ -1,5 +1,4 @@
 import { Sequelize, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize"
-import bcrypt from "bcryptjs"
 
 export const sequelizeInstance = new Sequelize("postgres://mimir:mimir-pwd@localhost:5432/test")
 
@@ -14,27 +13,23 @@ export interface UserPO extends Model<InferAttributes<UserPO>> {
     telephone: string | null;
 };
 
-export interface UserCreateReq extends Model<InferCreationAttributes<UserPO>> {
+/**
+ * 用户请求
+ */
+export interface UsercreateReq extends Model<InferCreationAttributes<UserPO>> {
     username: string;
     password: string;
     email: string | null;
     telephone: string | null;
- }
-
-export interface UserVO {
-    id: number;
-    username: string;
-    email?: string | null;
-    telephone?: string | null;
 }
 
-export function toVO(po: UserPO): UserVO {
-    return {
-        id: po.id!,
-        username: po.username,
-        email: po.email,
-        telephone: po.telephone,
-    };
+/**
+ * 用户请求
+ */
+export interface UserUpdateReq {
+    username: string;
+    email: string | null;
+    telephone: string | null;
 }
 
 /**
@@ -55,9 +50,6 @@ export const UserModel = sequelizeInstance.define<UserPO>(
         password: {
             type: DataTypes.STRING(256),
             allowNull: false,
-            set(val: string) {
-                this.setDataValue("password", bcrypt.hashSync(val));
-            },
         },
         email: {
             type: DataTypes.STRING(128),
@@ -66,11 +58,12 @@ export const UserModel = sequelizeInstance.define<UserPO>(
         telephone: {
             type: DataTypes.STRING(32),
             allowNull: true,
-        }
+        },
     },
     {
         tableName: "user",
         underscored: true,
+        timestamps: true,
     }
 );
 await UserModel.sync();
--- setup.ts
+++ setup.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { createLogger } from "@liplum/log";
-import { toVO, UserCreateReq, UserModel } from "./model.js";
+import { UserUpdateReq, UserModel, UsercreateReq } from "./model.js";
+import bcrypt from "bcryptjs"
 
 export const log = createLogger();
 export const app: express.Express = express();
@@ -14,14 +15,38 @@ const v1 = express.Router();
 app.use("/v1", v1);
 v1.use("/user", userRouter);
 
-userRouter.post("", async (req, res) => {
-    const userCreateReq: UserCreateReq = req.body;
+userRouter.post("/", async (req, res) => {
+    const userCreateReq: UsercreateReq = req.body;
+    userCreateReq.password = await bcrypt.hash(userCreateReq.password, 10);
     await UserModel.create(userCreateReq)
         .then((userPO) => {
-            res.status(201).json(toVO(userPO));
+            res.status(201).json({
+                id: userPO.id,
+                username: userPO.username,
+                email: userPO.email,
+                telephone: userPO.telephone,
+            });
         })
         .catch((error) => {
             log.error(error);
             res.status(500).json({ error: "Internal Server Error" });
+        })
+})
+
+userRouter.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    const userUpdateReq: UserUpdateReq = req.body;
+    await UserModel.update(userUpdateReq, { where: { id }, returning: true })
+        .then((result) => {
+            res.status(200).json({
+                id: result[1][0].id,
+                username: result[1][0].username,
+                email: result[1][0].email,
+                telephone: result[1][0].telephone
+            })
+        })
+        .catch((error) => {
+            log.error(error);
+            res.status(500).json({ error: "Internal Server Error" })
         })
 })
